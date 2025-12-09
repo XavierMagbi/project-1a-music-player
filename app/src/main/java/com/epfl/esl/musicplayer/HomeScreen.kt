e930f35f49847f2b0018a183302a7ec16e011e4a
Olivier Dadoud <oliver.dad@tinquiete.com>
2025-12-09 11:18:31 +0100

    Navigation and Drawer

---
+++ app/src/main/java/com/epfl/esl/musicplayer/HomeScreen.kt
@@ -0,0 +1,37 @@
+package com.epfl.esl.musicplayer
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(onPlayerClicked: () -> Unit,modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick=onPlayerClicked){
+                Text("Play")
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen({})
+    
+}
+
