d8834705e450b5fa7eea14d7dadd20157c81d594
Thomas Lenges <thomas.lenges@epfl.ch>
2026-01-16 12:15:15 +0100

    Search bar to look for friends

--- app/src/main/java/com/epfl/esl/musicplayer/HomeScreen.kt
+++ app/src/main/java/com/epfl/esl/musicplayer/HomeScreen.kt
@@ -3,15 +3,27 @@ package com.epfl.esl.musicplayer
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(
@@ -20,12 +32,37 @@ fun HomeScreen(
         modifier = modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Column(
+        // For research query
+        var searchQuery by remember { mutableStateOf("") }
+
+        LazyColumn(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Welcome to WristWave!")
+            item{
+                Text("Welcome to WristWave!")
+            }
+
+            item {
+                OutlinedTextField(
+                    value = searchQuery,
+                    onValueChange = { searchQuery = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    placeholder = { Text("Search friends...") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search friends"
+                        )
+                    },
+                    singleLine = true
+                )
+            }
         }
     }
+
+
 
 }
 
