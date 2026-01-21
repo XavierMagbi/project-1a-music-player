d1c80bab8d4935903c776c4583d6b3b7bea6c9d2
Thomas Lenges <thomas.lenges@epfl.ch>
2026-01-21 13:36:41 +0100

    Delete factory in PlayScreen to compile

--- app/src/main/java/com/epfl/esl/musicplayer/PlayScreenViewModel.kt
+++ app/src/main/java/com/epfl/esl/musicplayer/PlayScreenViewModel.kt
@@ -338,25 +338,4 @@ class PlayScreenViewModel (
         playCurrentTrack()
 
     }
-
-    class PlayScreenViewModelFactory(
-        private val playlistId: String,
-        private val application: Application
-    ) : ViewModelProvider.Factory {
-
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            if (modelClass.isAssignableFrom(SelectedPlaylistViewModel::class.java)) {
-                @Suppress("UNCHECKED_CAST")
-                return SelectedPlaylistViewModel( playlistId =playlistId, application = application ) as T
-            }
-            throw IllegalArgumentException("Unknown ViewModel class")
-        }
-    }
-
-
-
-
-
-
-
 }
