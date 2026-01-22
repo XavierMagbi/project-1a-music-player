c5b7bf8e96b39b5c06964a303ceb28735614329e
Thomas Lenges <thomas.lenges@epfl.ch>
2026-01-22 10:55:02 +0100

    Data classes correction

--- app/src/main/java/com/epfl/esl/musicplayer/MainActivity.kt
+++ app/src/main/java/com/epfl/esl/musicplayer/MainActivity.kt
@@ -587,22 +587,7 @@ class MainActivity : ComponentActivity(),MessageClient.OnMessageReceivedListener
 
 }
 
-// ==== Data Classes ====
 
-// Data class to hold user profile information to be displayed in HomeScreen
-data class UserProfile(
-    val username: String,
-    val image: Bitmap? = null
-)
-
-// Data class to hold music metadata to be displayed in DiscoverScreen and used for playback
-data class musicMetadata (
-    val title: String? = "",
-    val image: ByteArray? = null,
-    val link: String? = "",
-    val linkGS: String? = "",
-    val datapath: String=""
-)
 
 
 
