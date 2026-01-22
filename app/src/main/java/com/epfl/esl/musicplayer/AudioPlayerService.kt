97fc178556b4dec3fd5853d70210a6881c887fef
Thomas Lenges <thomas.lenges@epfl.ch>
2026-01-22 15:49:16 +0100

    AudioPlayerService commented

--- app/src/main/java/com/epfl/esl/musicplayer/AudioPlayerService.kt
+++ app/src/main/java/com/epfl/esl/musicplayer/AudioPlayerService.kt
@@ -13,40 +13,57 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
+/*
+    Audio Player Service
+    Called from PlayScreenViewModel exclusively
+
+    Functionality:
+    Handles audio playback using Android MediaPlayer API
+
+    Interacts with:
+    Android MediaPlayer API - to play, pause, seek audio tracks
+ */
+
 class AudioPlayerService (
     private val context: Context
 ){
+    // Service variables
     private var mediaPlayer: MediaPlayer? = null
     private var updateJob: Job? = null
+    var onCompletionListener: (() -> Unit)? = null
 
+    // Service LiveData variables
+    // Is music currently playing
     private val _isPlaying = MutableLiveData<Boolean?>()
     val isPlaying: LiveData<Boolean?>
         get() = _isPlaying
 
+    // Current music position
     private val _currentPosition = MutableLiveData<Int>(0)
     val currentPosition: LiveData<Int>
         get() = _currentPosition
 
+    // Overall music duration
     private val _duration = MutableLiveData<Int>(0)
     val duration: LiveData<Int>
         get() = _duration
 
+    // Music title set in .mp3 metadata
     private val _title = MutableLiveData<String>("")
     val title: LiveData<String>
         get() = _title
 
+    // Music cover image set in .mp3 metadata
     private val _cover = MutableLiveData<ByteArray?>(null)
     val cover: LiveData<ByteArray?>
         get() = _cover
-
-    var onCompletionListener: (() -> Unit)? = null
 
     // For equalizer
     private val _audioSessionId = MutableLiveData<Int?>(null)
     val audioSessionId: LiveData<Int?>
         get() = _audioSessionId
 
-
+    // Service functions
     fun play(musicResId: String){
         // To stop playing the initial song when switching to another one
         mediaPlayer?.let {
@@ -57,17 +74,16 @@ class AudioPlayerService (
         }
         mediaPlayer = null
 
-        // To extract metadata through URI Android
+        // To extract metadata for PlayScreen
         val retriever = MediaMetadataRetriever()
         try {
-            // Get URI
-            //val uri = Uri.parse("android.resource://${context.packageName}/$musicResId")
+            // Set up retriever
             val uri = Uri.parse(musicResId)
             retriever.setDataSource(context, uri)
-            // Get title
+            // Extract title
             val metaTitle = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE)
             _title.value = metaTitle ?: "Unknown"
-            // Get cover image
+            // Extract cover image
             val metaCover = retriever.embeddedPicture
             _cover.value = metaCover
         } catch (e: Exception) {
@@ -77,16 +93,17 @@ class AudioPlayerService (
             retriever.release()
         }
 
-        // To play the music
-        mediaPlayer = MediaPlayer.create(context, Uri.parse(musicResId)) // Requires APK hence Context to access musics
-        // For equalizer
+        // Set up MediaPlayer
+        mediaPlayer = MediaPlayer.create(context, Uri.parse(musicResId)) 
+        // Fetch audio session ID for equalizer
         _audioSessionId.value = mediaPlayer?.audioSessionId
+        // Start playback
         mediaPlayer?.start()
         _isPlaying.value = true
-        startUpdatingProgress()
-        _duration.value = mediaPlayer?.duration ?: 0
+        startUpdatingProgress() // For music time progression tracking
+        _duration.value = mediaPlayer?.duration ?: 0 // Get duration for slider
 
-        // To handle end of music
+        // Handle end of music playback
         mediaPlayer?.setOnCompletionListener {
             _isPlaying.value = false
             stopUpdatingProgress()
@@ -94,28 +111,33 @@ class AudioPlayerService (
         }
     }
 
+    // Handle pause in music playback
     fun pause(){
         mediaPlayer?.pause()
         _isPlaying.value = false
         stopUpdatingProgress()
     }
 
+    // Handle resume in music playback
     fun resume(){
         mediaPlayer?.start()
         _isPlaying.value = true
         startUpdatingProgress()
     }
 
+    // Handle rewind in music playback
     fun rewind(){
         mediaPlayer?.seekTo(0)
         _currentPosition.postValue(0)
     }
 
+    // Handle slider user movement in music playback
     fun seekTo(position: Int){
         mediaPlayer?.seekTo(position)
         _currentPosition.postValue(position)
     }
 
+    // Update current position every 500ms when music is playing
     private fun startUpdatingProgress() {
         updateJob?.cancel()
         updateJob = CoroutineScope(Dispatchers.Main).launch {
@@ -130,11 +152,8 @@ class AudioPlayerService (
         }
     }
 
+    // Stop updating current position
     private fun stopUpdatingProgress() {
         updateJob?.cancel()
-    }
-
-    fun changeQueue(songIdx:Int,newQueue:List<Int>){
-
     }
 }
